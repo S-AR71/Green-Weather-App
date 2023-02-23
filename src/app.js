bd74ffa3917a28db84b172e8ec5aefa7450f1f6f
S-AR71 <116632150+S-AR71@users.noreply.github.com>
2023-02-23 18:25:56 +0100

    JS Added

--- src/app.js
+++ src/app.js
@@ -1,4 +1,18 @@
-let apiKey = "3a60oc81taf3cb21592f1703fcb844bb";
-let apiUrl = `https://api.shecodes.io/weather/v1/current?query={Lisbon}&key=${apiKey}`;
+function displayTemperature(response) {
+  console.log(response.data);
+  let temperatureElement = document.querySelector("#temperature");
+  let cityElement = document.querySelector("#city");
+  let descriptionElement = document.querySelector("#description");
+  let humidityElement = document.querySelector("#humidity");
+  let windElement = document.querySelector("#wind");
+  temperatureElement.innerHTML = Math.round(response.data.temperature.current);
+  cityElement.innerHTML = response.data.city;
+  descriptionElement.innerHTML = response.data.condition.description;
+  humidityElement.innerHTML = response.data.temperature.humidity;
+  windElement.innerHTML = response.data.wind.speed;
+}
 
-console.log(apiKey);
+let apiKey = "3a60oc81taf3cb21592f1703fcb844bb";
+let apiUrl = `https://api.shecodes.io/weather/v1/current?query=Lisbon&key=${apiKey}&units=metric`;
+
+axios.get(apiUrl).then(displayTemperature);
