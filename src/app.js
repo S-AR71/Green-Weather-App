b659b86baee47c80605e227feed84df066f95647
S-AR71 <116632150+S-AR71@users.noreply.github.com>
2023-02-26 19:39:02 +0100

    JS Changes

--- src/app.js
+++ src/app.js
@@ -23,12 +23,14 @@ function formatDate(timestamp) {
 }
 
 function displayTemperature(response) {
+  console.log(response.data);
   let temperatureElement = document.querySelector("#temperature");
   let cityElement = document.querySelector("#city");
   let descriptionElement = document.querySelector("#description");
   let humidityElement = document.querySelector("#humidity");
   let windElement = document.querySelector("#wind");
   let dateElement = document.querySelector("#date");
+
   temperatureElement.innerHTML = Math.round(response.data.temperature.current);
   cityElement.innerHTML = response.data.city;
   descriptionElement.innerHTML = response.data.condition.description;
@@ -36,9 +38,10 @@ function displayTemperature(response) {
   windElement.innerHTML = response.data.wind.speed;
   wind.innerHTML = Math.round(response.data.wind.speed);
   dateElement.innerHTML = formatDate(response.data.time * 1000);
+
+  let apiKey = "3a60oc81taf3cb21592f1703fcb844bb";
+  let city = "Lisbon";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+
+  axios.get(apiUrl).then(displayTemperature);
 }
-
-let apiKey = "3a60oc81taf3cb21592f1703fcb844bb";
-let apiUrl = `https://api.shecodes.io/weather/v1/current?query=Lisbon&key=${apiKey}&units=metric`;
-
-axios.get(apiUrl).then(displayTemperature);
