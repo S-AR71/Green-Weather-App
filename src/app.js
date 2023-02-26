27098fe129cf58ecb03f64faf9b8d845dd5bb3d1
S-AR71 <116632150+S-AR71@users.noreply.github.com>
2023-02-26 22:01:40 +0100

    formatDay

--- src/app.js
+++ src/app.js
@@ -22,7 +22,16 @@ function formatDate(timestamp) {
   return `${day} ${hours}:${minutes}`;
 }
 
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let day = date.getDay();
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+  return days[day];
+}
+
 function displayTemperature(response) {
+  console.log(response.data);
   let temperatureElement = document.querySelector("#temperature");
   let cityElement = document.querySelector("#city");
   let descriptionElement = document.querySelector("#description");
